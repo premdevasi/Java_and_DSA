1c2019985600752bb4dcd4aceee3d8a3e840d134
prem devasi <officialprem9509@gmail.com>
2024-03-18 18:24:49 +0530

    question one is done

---
+++ arrays_and_arrayList/assignments_arrays_arrayList.java
@@ -0,0 +1,34 @@
+package arrays_and_arrayList;
+
+import java.util.*;
+
+class assignments_arrays_arrayList {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // Build Array from Permutation
+        // System.out.print("Enter n: ");
+        // int n = in.nextInt();
+        // System.out.print("Enter r: ");
+        // int r = in.nextInt();
+
+        // int ans = Permutation(n, r);
+        // System.out.println(ans);
+
+    }
+
+    // Build Array from Permutation
+    // static int Permutation(int n, int r) {
+    //     int Permutation = Factorial(n) / Factorial(n - r);
+    //     return Permutation;
+    // }
+
+    // Factorial
+    // static int Factorial(int n) {
+    //     int fact = 1;
+    //     for (int i = 1; i <= n; i++) {
+    //         fact = fact * i;
+    //     }
+    //     return fact;
+    // }
+}
